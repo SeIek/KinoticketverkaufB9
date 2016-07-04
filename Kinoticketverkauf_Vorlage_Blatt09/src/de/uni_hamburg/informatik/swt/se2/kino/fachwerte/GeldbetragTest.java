e453bcd572e9e165106de7ce0d57b4ff81f3f1be
5sandmei <5sandmei@rzpc144.informatik.uni-hamburg.de>
2016-07-04 17:36:27 +0200

    Testklasse

---
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -0,0 +1,95 @@
+package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class GeldbetragTest
+{
+    Geldbetrag _betrag100;
+    Geldbetrag _betrag250;
+    Geldbetrag _betrag500a;
+    Geldbetrag _betrag500b;
+    Geldbetrag _betrag400;
+    Geldbetrag _betrag1000;
+    Geldbetrag _betragMinus100;
+    Geldbetrag _betragMinus400;
+    Geldbetrag _betragMinus500;
+
+    public GeldbetragTest()
+    {
+        _betrag100 = Geldbetrag.getGeldbetrag(100);
+        _betrag250 = Geldbetrag.getGeldbetrag(250);
+        _betrag400 = Geldbetrag.getGeldbetrag(400);
+        _betrag500a = Geldbetrag.getGeldbetrag(500);
+        _betrag500b = Geldbetrag.getGeldbetrag(500);
+        _betrag1000 = Geldbetrag.getGeldbetrag(1000);
+        _betragMinus100 = Geldbetrag.getGeldbetrag(-100);
+        _betragMinus400 = Geldbetrag.getGeldbetrag(-400);
+        _betragMinus500 = Geldbetrag.getGeldbetrag(-500);
+    }
+
+    @Test
+    void testeEqualsundHash()
+    {
+        assertEquals(_betrag500a.hashCode(), _betrag500b.hashCode());
+        assertEquals(_betrag500a.hashCode(), _betrag500a.hashCode());
+        assertEquals(_betrag500a, _betrag500b);
+        assertFalse(_betrag500a.equals(_betrag100));
+    }
+
+    @Test
+    void testeAddieren()
+    {
+        assertEquals(_betrag1000, Geldbetrag.addiere(_betrag500a, _betrag500b));
+        assertEquals(_betrag1000, Geldbetrag.addiere(_betrag500a, _betrag500a));
+        assertEquals(_betrag500a, Geldbetrag.addiere(_betrag250, _betrag250));
+        assertEquals(_betrag400,
+                Geldbetrag.addiere(_betrag500a, _betragMinus100));
+
+    }
+
+    @Test
+    void testeSubtrahieren()
+    {
+        assertEquals(_betrag400,
+                Geldbetrag.subtrahiere(_betrag500a, _betrag100));
+        assertEquals(_betrag250,
+                Geldbetrag.subtrahiere(_betrag500a, _betrag250));
+        assertEquals(_betrag1000,
+                Geldbetrag.subtrahiere(_betrag500a, _betragMinus500));
+        assertEquals(_betragMinus400,
+                Geldbetrag.subtrahiere(_betrag100, _betrag500a));
+
+    }
+
+    @Test
+    void testeMultiplizieren()
+    {
+        assertEquals(_betrag500a, Geldbetrag.multipliziere(_betrag100, 5));
+        assertEquals(_betrag500a, Geldbetrag.multipliziere(_betrag250, 2));
+        assertEquals(_betragMinus500, Geldbetrag.multipliziere(_betrag100, -5));
+        assertEquals(_betragMinus500,
+                Geldbetrag.multipliziere(_betragMinus100, 5));
+    }
+
+    @Test
+    void testKonvertiereToGeldbetrag()
+    {
+        assertTrue(Geldbetrag.istGeldbetrag("1,00"));
+        assertTrue(Geldbetrag.istGeldbetrag("1"));
+        assertFalse(Geldbetrag.istGeldbetrag("a"));
+        assertFalse(Geldbetrag.istGeldbetrag("1.00"));
+
+        assertEquals(_betrag100, Geldbetrag.getGeldbetrag("1,00"));
+        assertEquals(_betrag100, Geldbetrag.getGeldbetrag("1"));
+    }
+
+    @Test
+    void testeIstPositiv()
+    {
+        assertTrue(_betrag500.istPositiv());
+    }
+}
--- Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/startup/StartupKinoticketverkauf.java
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/startup/StartupKinoticketverkauf.java
@@ -48,8 +48,8 @@ public class StartupKinoticketverkauf
      */
     private static Kino erzeugeKinoMitBeispieldaten()
     {
-        final Kinosaal[] saele = { new Kinosaal("Saal 1", 20, 25),
-                new Kinosaal("Saal 2", 16, 20), new Kinosaal("Saal 3", 10, 16) };
+        final Kinosaal[] saele = {new Kinosaal("Saal 1", 20, 25),
+                new Kinosaal("Saal 2", 16, 20), new Kinosaal("Saal 3", 10, 16)};
 
         // Filme: Top-5 Deutschland laut kino.de in der Kalenderwoche 20, 2011.
         Film[] filme = {
@@ -58,12 +58,14 @@ public class StartupKinoticketverkauf
                 new Film("Fast & Furious Five", 130, FSK.FSK12, true),
                 new Film("Rio", 96, FSK.FSK0, false),
                 new Film("Wasser für die Elefanten", 120, FSK.FSK12, false),
-                new Film("Thor", 115, FSK.FSK12, false) };
+                new Film("Thor", 115, FSK.FSK12, false)};
 
         Uhrzeit nachmittag = Uhrzeit.get(17, 30);
         Uhrzeit abend = Uhrzeit.get(20, 0);
         Uhrzeit spaet = Uhrzeit.get(22, 30);
         Uhrzeit nacht = Uhrzeit.get(1, 0);
+
+        //        Geldbetrag guenstig = Geldbetrag.get(1,0); XXX Möglichkeit 1
 
         Datum d1 = Datum.heute();
         Datum d2 = d1.naechsterTag();
@@ -71,54 +73,36 @@ public class StartupKinoticketverkauf
 
         final Vorstellung[] vorstellungen = {
                 // Heute
-                new Vorstellung(saele[0], filme[2], nachmittag, abend, d1,
-                        500),
-                new Vorstellung(saele[0], filme[0], abend, spaet, d1,
-                        700),
-                new Vorstellung(saele[0], filme[0], spaet, nacht, d1,
-                        700),
+                new Vorstellung(saele[0], filme[2], nachmittag, abend, d1, 500),
+                new Vorstellung(saele[0], filme[0], abend, spaet, d1, 700),
+                new Vorstellung(saele[0], filme[0], spaet, nacht, d1, 700),
 
-                new Vorstellung(saele[1], filme[3], nachmittag, abend, d1,
-                        900),
-                new Vorstellung(saele[1], filme[1], spaet, nacht, d1,
-                        800),
+                new Vorstellung(saele[1], filme[3], nachmittag, abend, d1, 900),
+                new Vorstellung(saele[1], filme[1], spaet, nacht, d1, 800),
 
-                new Vorstellung(saele[2], filme[3], abend, spaet, d1,
-                        1000),
-                new Vorstellung(saele[2], filme[4], spaet, nacht, d1,
-                        900),
+                new Vorstellung(saele[2], filme[3], abend, spaet, d1, 1000),
+                new Vorstellung(saele[2], filme[4], spaet, nacht, d1, 900),
 
                 // Morgen
-                new Vorstellung(saele[0], filme[0], abend, spaet, d2,
-                        500),
-                new Vorstellung(saele[0], filme[0], spaet, nacht, d2,
-                        700),
+                new Vorstellung(saele[0], filme[0], abend, spaet, d2, 500),
+                new Vorstellung(saele[0], filme[0], spaet, nacht, d2, 700),
 
-                new Vorstellung(saele[1], filme[2], nachmittag, abend, d2,
-                        900),
-                new Vorstellung(saele[1], filme[4], abend, nacht, d2,
-                        800),
+                new Vorstellung(saele[1], filme[2], nachmittag, abend, d2, 900),
+                new Vorstellung(saele[1], filme[4], abend, nacht, d2, 800),
 
                 new Vorstellung(saele[2], filme[3], nachmittag, abend, d2,
                         1000),
-                new Vorstellung(saele[2], filme[1], spaet, nacht, d2,
-                        900),
+                new Vorstellung(saele[2], filme[1], spaet, nacht, d2, 900),
 
                 // Übermorgen
-                new Vorstellung(saele[0], filme[1], abend, spaet, d3,
-                        500),
-                new Vorstellung(saele[0], filme[1], spaet, nacht, d3,
-                        700),
+                new Vorstellung(saele[0], filme[1], abend, spaet, d3, 500),
+                new Vorstellung(saele[0], filme[1], spaet, nacht, d3, 700),
 
-                new Vorstellung(saele[1], filme[2], nachmittag, abend, d3,
-                        900),
-                new Vorstellung(saele[1], filme[0], abend, nacht, d3,
-                        800),
+                new Vorstellung(saele[1], filme[2], nachmittag, abend, d3, 900),
+                new Vorstellung(saele[1], filme[0], abend, nacht, d3, 800),
 
-                new Vorstellung(saele[2], filme[3], abend, spaet, d3,
-                        1000),
-                new Vorstellung(saele[2], filme[4], spaet, nacht, d3,
-                        900) };
+                new Vorstellung(saele[2], filme[3], abend, spaet, d3, 1000),
+                new Vorstellung(saele[2], filme[4], spaet, nacht, d3, 900)};
 
         return new Kino(saele, vorstellungen);
     }
